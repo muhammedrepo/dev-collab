4ac298e5f5a5094146f29f36dc37298024577037
Muhammed Tijani <coolzyte@gmail.com>
2024-04-15 06:15:45 +0100

    Finished setting up google auth

---
+++ src/components/header.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { ModeToggle } from './mode-toggle'
+import { Button } from './ui/button'
+
+export function Header() {
+  const session = useSession()
+
+  return (
+    <header>
+      <div>
+        {session.data ? (
+          <Button onClick={() => signOut()}>Sign Out</Button>
+        ) : (
+          <Button onClick={() => signIn('google')}>Sign In</Button>
+        )}
+        {session.data?.user?.name}
+        <ModeToggle />
+      </div>
+    </header>
+  )
+}
